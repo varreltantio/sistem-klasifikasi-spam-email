acc5592ceda2bda18083dac51577d0063785e398
varreldt <varreldtantio@gmail.com>
2022-11-15 01:34:14 +0700

    add model indo

--- TextPreprocessing.py
+++ TextPreprocessing.py
@@ -2,24 +2,36 @@ import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-
+import spacy
+from spacy.language import Language
+from spacy_langdetect import LanguageDetector
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
 class Model(object):
 
-    def __init__(self, text):
-        self.text = text
+    def __init__(self):
+        self.text = None
+        self.language = None
 
-        self.text = self.lowercase(self.text)
-        self.text = self.preprocess_remove(self.text)
-        self.text = self.stopwordsRemove(self.text)
-        self.text = self.lemmatization(self.text)
+    def preprocess(self, text):
+        self.text = self.lowercase(text)
+        self.text = self.textCleansing(self.text)
+        self.text, self.language = self.detectLanguage(self.text)
+        self.text = self.stopwordsRemove(self.text, self.language)
+
+        if (self.language == 'en'):
+            self.text = self.lemmatization(self.text)
+        else:
+            self.text = self.stemming(self.text)
 
         self.text = str(' '.join(self.text))
 
-    def lowercase(self, message):
-        return message.lower()
+        return self.text, self.language
 
-    def preprocess_remove(self, text):
+    def lowercase(self, text):
+        return text.lower()
+
+    def textCleansing(self, text):
         text = re.sub(r'@[a-zA-Z0-9_]+', '', text)
         text = re.sub(r'https?://[A-Za-z0-9./]+', '', text)
         text = re.sub(r'www.[^ ]+', '', text)
@@ -29,18 +41,39 @@ class Model(object):
         text = ' '.join(text)
         return text
 
-    def stopwordsRemove(self, inputs):
-        stop_words = set(stopwords.words('english'))
-        stop_words.remove('not')
+    def get_lang_detector(self, nlp, name):
+        return LanguageDetector()
 
-        word_tokens = word_tokenize(inputs)
+    def detectLanguage(self, text):
+        nlp = spacy.load("en_core_web_sm")
+        Language.factory("language_detector", func=self.get_lang_detector)
+        nlp.add_pipe('language_detector', last=True)
 
+        doc = nlp(text)
+        language = doc._.language['language']
+        return text, language
+
+    def stopwordsRemove(self, text, language):
+        if language == 'en':
+            stop_words = set(stopwords.words('english'))
+            stop_words.remove('not')
+        else:
+            stop_words = stopwords.words('indonesian')
+
+        word_tokens = word_tokenize(text)
         filtered_text = [
             word for word in word_tokens if word not in stop_words]
         return filtered_text
 
-    def lemmatization(self, inputs):
+    def lemmatization(self, text):
         lemmatizer = WordNetLemmatizer()
 
-        lemmas = [lemmatizer.lemmatize(word=x, pos='v') for x in inputs]
+        lemmas = [lemmatizer.lemmatize(word=x, pos='v') for x in text]
         return lemmas
+
+    def stemming(self, text):
+        factory = StemmerFactory()
+        stemmer = factory.create_stemmer()
+
+        stemmed_tokens = [stemmer.stem(token) for token in text]
+        return stemmed_tokens
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-import TextPreprocessing
+from TextPreprocessing import Model
 from flask import Flask, render_template, jsonify, request
 import joblib
 
@@ -16,16 +16,24 @@ def predict():
         json_ = request.get_json()
 
         message = str(json_['message'])
-        clean_message = TextPreprocessing.Model(message)
+        preprocess_model = Model()
+        clean_message, language = preprocess_model.preprocess(message)
 
-        # TF - IDF
-        tf_idf = joblib.load("tf_idf.save")
+        if (language == 'en'):
+            # TF - IDF
+            tf_idf = joblib.load("model/tf_idf_en.save")
 
-        # Naive Bayes Classifier Model
-        model = joblib.load("nb.pkl")
+            # Naive Bayes Classifier Model
+            model = joblib.load("model/nb_en.pkl")
+        else:
+            # TF - IDF
+            tf_idf = joblib.load("model/tf_idf_id.save")
+
+            # Naive Bayes Classifier Model
+            model = joblib.load("model/nb_id.pkl")
 
         # Get prediction
-        data = tf_idf.transform([clean_message.text]).toarray()
+        data = tf_idf.transform([clean_message]).toarray()
 
         prediction = model.predict(data)[0]
 
